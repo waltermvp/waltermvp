3628f939844eaf5830d44c7218f197f5be8137c8
waltermvp <walter.vargaspena@gmail.com>
2023-01-24 14:37:47 -0500

    amplify re-init

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "waltermvp": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  },
-  "function": {
-    "userCreated": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
